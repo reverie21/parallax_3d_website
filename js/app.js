8145f0b5d7462767ac00571e0e2aa314965b74b1
My Name <me@example.com>
2023-05-16 16:36:25 -0500

    Add initial parallax movement

--- js/app.js
+++ js/app.js
@@ -7,4 +7,12 @@ let xValue = 0,
 window.addEventListener("mousemove", (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
+
+    parallax_el.forEach(el => {
+        let speedx = el.dataset.speedx;
+        let speedy = el.dataset.speedy;
+        el.style.transform = `translateX(calc(-50% + ${xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`;
+    })
+
+    console.log(xValue, yValue);
 })
